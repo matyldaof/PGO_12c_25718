f76f3aeb728ebc111e330f9fd78978b2ef51de8c
Matylda <s25718@pjwstk.edu.pl>
2022-04-08 19:47:47 +0200

    commit

---
+++ src/cwiczenia2/zad1/zad1.java
@@ -0,0 +1,13 @@
+package cwiczenia2.zad1;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class zad1 {
+
+    public class Vehicle{
+       private String name;
+       private LocalDate date;
+       private List<Integer> wheels;
+    }
+}
